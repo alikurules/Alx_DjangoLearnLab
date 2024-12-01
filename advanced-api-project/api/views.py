1306ee901d19d96a8343307fd5ff788ea88a05b4
alikurules <alikurules@gmail.com>
2024-12-01 20:30:44 +0300

    Advanced API project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework
+from django_filters import rest_framework, filters.OrderingFilter
 
 # List all books
 class BookListView(generics.ListAPIView):
