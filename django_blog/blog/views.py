bd096165711a7e3d0c243a396e63a51fb0b01f8f
alikurules <alikurules@gmail.com>
2024-12-15 07:35:27 +0300

    Added User Profiles

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+
+
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+
+    return render(request, 'registration/profile.html', {'form': form})
