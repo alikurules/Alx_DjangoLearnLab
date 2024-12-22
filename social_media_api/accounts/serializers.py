c0e1ec7a10196b40ae6e26b7026b23367b0bf671
alikurules <alikurules@gmail.com>
2024-12-22 17:59:13 +0300

    Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,11 @@
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 
 # Get the custom user model
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializer for user data."""
+    """Serializer for retrieving user details."""
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
@@ -17,15 +16,13 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'email']
+        fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        """Create a new user and generate a token."""
+        """Create a new user with hashed password."""
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
-        # Generate an authentication token for the new user
-        Token.objects.create(user=user)
         return user
